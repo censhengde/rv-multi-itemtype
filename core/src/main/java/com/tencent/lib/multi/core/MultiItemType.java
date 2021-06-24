14487c059fc391d15617481adafa91c0b496cd85
v_sdecen <v_sdecen@tencent.com>
2021-06-24 11:28:48 +0800

    --all

---
+++ core/src/main/java/com/tencent/lib/multi/core/MultiItemType.java
@@ -0,0 +1,24 @@
+package com.tencent.lib.multi.core;
+
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Author：岑胜德 on 2021/6/23 19:14
+ *
+ * 说明：
+ */
+public abstract class MultiItemType<T> extends ItemTypeImpl<T,MultiViewHolder> {
+
+    /**
+     * 这里需要重写onCreateViewHolder方法，否则 MultiItemType 的子类无法隔代反射到 VH 参数
+     * @param parent
+     * @return
+     */
+    @NonNull
+    @Override
+    public MultiViewHolder onCreateViewHolder(@NonNull ViewGroup parent) {
+        return MultiViewHolder.create(parent.getContext(),parent,getItemLayoutRes());
+    }
+}
