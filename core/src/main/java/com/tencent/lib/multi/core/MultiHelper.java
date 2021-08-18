7c02bff262b9ab7bc3495e47fd5f817cc75a57bf
v_sdecen <v_sdecen@tencent.com>
2021-08-18 19:10:07 +0800

    废除mItemTypeRecord字段

--- core/src/main/java/com/tencent/lib/multi/core/MultiHelper.java
+++ core/src/main/java/com/tencent/lib/multi/core/MultiHelper.java
@@ -25,12 +25,7 @@ public abstract class MultiHelper<T, VH extends RecyclerView.ViewHolder> {
      * ItemType集合，以其id为key，ItemType为value。
      */
     private final SparseArray<ItemType<T, VH>> mItemTypes = new SparseArray<>();
-    /**
-     * ItemType在Adapter position上的对应记录，其索引与Adapter position一一对应,
-     * 表示某position想要表现的ItemType，
-     * 注意，并非一定与数据集的index对应。
-     */
-    private List<ItemType<T, VH>> mItemTypeRecord;
+
 
 
     public MultiHelper(Adapter realAdapter) {
@@ -46,45 +41,9 @@ public abstract class MultiHelper<T, VH extends RecyclerView.ViewHolder> {
         if (position == RecyclerView.NO_POSITION) {
             return INVALID_VIEW_TYPE;
         }
-        ItemType<T, VH> currentType = null;
-        final int typeSize = mItemTypes.size();
-        //单样式
-        if (typeSize == 1) {
-            currentType = mItemTypes.valueAt(0);
-        }
-        //多样式
-        else if (typeSize > 1) {
-            final T data = getItem(position);
 
-            //mItemTypeRecord 初始化在这里进行
-            if (mItemTypeRecord == null) {
-                mItemTypeRecord = new ArrayList<>();
-            }
-            if (position >= 0 && position < mItemTypeRecord.size()) {
-                currentType = mItemTypeRecord.get(position);
-                //如果当前 position 对应的ItemType不再与旧的ItemType匹配，
-                // 说明当前 position 对应的ItemType已经被更改，在RecyclerView列表进行增、删、改操作
-                // 时候可能会出现这种情况，这时候就需重新匹配当前position所对应的ItemType
-                if (!currentType.matchItemType(data, position)) {
-
-                    final ItemType<T, VH> newType = findCurrentType(data, position);
-                    if (newType != null) {
-                        currentType = newType;
-                        mItemTypeRecord.set(position, newType);
-                    }
-
-                }
-            }
-            //首次进来 mItemTypeRecord.isEmpty() 为true，会走这里。
-            else {
-                currentType = findCurrentType(data, position);
-                if (currentType != null) {
-                    mItemTypeRecord.add(currentType);
-                }
-
-            }
-        }
-
+        final T data = getItem(position);
+        final ItemType<T, VH> currentType = findCurrentType(data, position);
         return currentType == null ? INVALID_VIEW_TYPE : currentType.getClass().hashCode();
     }
 
@@ -97,7 +56,7 @@ public abstract class MultiHelper<T, VH extends RecyclerView.ViewHolder> {
      */
     @Nullable
     private ItemType<T, VH> findCurrentType(T data, int position) {
-        //为当前position 匹配它的ItemType
+        //为当前position 匹配它的ItemType(一般一个页面item类型不会很多，遍历代价不大）
         for (int i = 0; i < mItemTypes.size(); i++) {
             final ItemType<T, VH> type = mItemTypes.valueAt(i);
             if (type.matchItemType(data, position)) {
@@ -123,22 +82,17 @@ public abstract class MultiHelper<T, VH extends RecyclerView.ViewHolder> {
             @NonNull List<Object> payloads) {
         /*统一捕获由position引发的可能异常*/
         try {
-            final int size = mItemTypes.size();
-            ItemType<T, VH> type;
-            if (size == 1) {/*单样式*/
-                type = mItemTypes.valueAt(0);
-            } else if (size > 1) {/*多样式*/
-                /*可能有越界风险*/
-                type = mItemTypeRecord.get(position);
-            } else {
+            final T data = getItem(position);
+            final ItemType<T, VH> currentType = findCurrentType(data, position);
+            if (currentType == null) {
                 return;
             }
             if (payloads.isEmpty()) {
-                type.onBindViewHolder(holder, this, position);
+                currentType.onBindViewHolder(holder, this, position);
             }
             /*局部刷新*/
             else {
-                type.onBindViewHolder(holder, this, position, payloads);
+                currentType.onBindViewHolder(holder, this, position, payloads);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -149,20 +103,9 @@ public abstract class MultiHelper<T, VH extends RecyclerView.ViewHolder> {
     @Nullable
     public abstract T getItem(int position);
 
-
-    /**
-     * 绝大多数情况下，用户不需要主动维护ItemTypeRecord集合，
-     * 即便当对item进行增、删、改（伴随数据集元素的增删改）。但用户应当拥有主动维护这个集合的权力。
-     * @return ItemType记录。当RecyclerView是单样式item的时候返回null。
-     */
-    @Nullable
-    public final List<ItemType<T, VH>> getItemTypeRecord() {
-        return mItemTypeRecord;
-    }
-
-
     /**
      * 注册ItemType
+     *
      * @param type
      * @return
      */
