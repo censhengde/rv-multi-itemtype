f1ef7eb4d98b307a32a9434d9ccdf658bc36f0b1
v_sdecen <v_sdecen@tencent.com>
2021-12-27 14:19:23 +0800

    修改代码细节

--- core/src/main/java/com/tencent/lib/multi/core/MultiItemManager.kt
+++ core/src/main/java/com/tencent/lib/multi/core/MultiItemManager.kt
@@ -4,7 +4,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
-import java.lang.reflect.Method
 import java.util.ArrayList
 
 /**
@@ -14,10 +13,12 @@ import java.util.ArrayList
  * 说明：实现Item多样式的公共逻辑封装。本质上是Adapter 生命周期的代理类，
  * 将 Adapter 生命周期分发给了position对应的ItemType。
  */
-abstract class MultiItemManager(val adapter: RecyclerView.Adapter<*>,
-                                val activity: FragmentActivity? = null,
-                                val fragment: Fragment? = null,
-                                 initialCapacity: Int = 0) {
+abstract class MultiItemManager(
+    val adapter: RecyclerView.Adapter<*>,
+    val activity: FragmentActivity? = null,
+    val fragment: Fragment? = null,
+    initialCapacity: Int = 0
+) {
     // ItemType 池.
     private val itemTypePool = ArrayList<ItemType<Any, RecyclerView.ViewHolder>>(initialCapacity)
 
@@ -35,26 +36,29 @@ abstract class MultiItemManager(val adapter: RecyclerView.Adapter<*>,
     private fun findCurrentItemViewType(data: Any?, position: Int): Int {
         itemTypePool.forEachIndexed { index, item ->
             if (item.isMatchForMe(data, position)) {
-                return index
+                return index // index 直接作为 itemViewType 返回。
             }
         }
-        throw java.lang.RuntimeException("Item view type not found!")
+        // 如果在当前 position 未匹配到对应的 ItemType，则抛异常，此时用户首先应当检查 ItemType 的
+        // isMatchForMe(...) 方法是否正确实现。
+        throw java.lang.RuntimeException("ItemType is not found in position:$position")
     }
 
     fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val item = itemTypePool[viewType]
-        return item.onCreateViewHolder(parent)
+        return itemTypePool[viewType].onCreateViewHolder(parent)
     }
 
-    fun onBindViewHolder(holder: RecyclerView.ViewHolder,
-                         position: Int,
-                         payloads: List<Any>) {
+    fun onBindViewHolder(
+        holder: RecyclerView.ViewHolder,
+        position: Int,
+        payloads: List<Any>
+    ) {
         if (position == RecyclerView.NO_POSITION) {
             return
         }
         val bean = getItem(position) ?: return
-        val currentItem = itemTypePool[holder.itemViewType]
-        currentItem.onBindViewHolder(holder, bean, position, payloads)
+        val type = itemTypePool[holder.itemViewType]
+        type.onBindViewHolder(holder, bean, position, payloads)
     }
 
     fun onViewRecycled(holder: RecyclerView.ViewHolder) {
